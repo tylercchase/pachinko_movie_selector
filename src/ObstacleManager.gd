6309b699703a98d0f5646219a4d8c25fd2ff1626
tylercchase <tylercchase@gmail.com>
2023-10-05 00:04:31 -0400

    init

---
+++ src/ObstacleManager.gd
@@ -0,0 +1,35 @@
+@tool
+extends Node3D
+
+@export var peg_scene: PackedScene
+
+@export var amount = 100 :
+	set(_amount):
+		if _amount != amount:
+			amount = _amount
+			_update_pegs()
+
+@export var rows = 10 :
+	set(_rows):
+		if _rows != rows:
+			rows = _rows
+			_update_pegs()
+
+
+
+
+func _update_pegs():
+	for child in get_children():
+		child.queue_free()
+	
+
+
+	for idx in amount:
+		var peg : Node3D = peg_scene.instantiate()
+		add_child.call_deferred(peg)
+		await peg.tree_entered
+		peg.set_global_position(Vector3(
+		-50 + floor(idx / rows) * (3.0 * 2),
+		45 - idx % rows * 3.0,
+		-2.5))
+	print(amount)
